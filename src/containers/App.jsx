5b00c670b3e85b3ebfcd58db2e687ebe2711f837
Sidd3100 <siddraimb@gmail.com>
2024-06-04 20:38:25 +0530

    Homepage updated

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from './Header';
+import Home from "./Home"
 
 const App = () => {
   return (
-    <div className="flex item-center justify-center">
-      <h1>Hi there</h1>
+    <div className="w-full xl:w-[1200px] py-32 px-4 lg:px-12 pr-4 lg:pr-32 text-white">
+      <Header/>
+      <Home/>
     </div>
   );
 };
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {HeroTypeWritter} from '../components'
+import {AnimatePresence,motion} from 'framer-motion'
+import {Hero} from "../assets/index.js"
 
 const Home = () => {
-  return <div>Home</div>;
+  return <>
+  <section id= "home" className = "flex items-center justify-center flex-col gap-12 relative">
+
+    <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-4">
+   <div className="w-full h-full flex flex-col items-center lg:items-start justify-center gap-4">
+    <h2 className="text-3xl lg:text-4xl text-texlight">
+      Hello, It's me 
+    <span className = "block tracking-wider text-4xl lf:text-6xl mt-4 text-white">
+      {" "}
+      Siddhartha Rai
+      </span>
+    </h2>
+    <h2 className="text-2xl lg:text-4xl text-texlight mt-4">
+      And I'm{" "}
+      <HeroTypeWritter
+      speed = {100}
+      words = {["a Developer..","an Youtuber..","a Freelancer.."]}
+      />
+    </h2>
+    <p className="text-base text-texlight mt-6 text-center lg:text-left">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ipsam. Quo repellendus natus error maiores qui vel eos commodi animi voluptate labore, nesciunt quia facere sapiente. Omnis unde inventore recusandae.
+    </p>
+    </div>
+    <div className="w-full h-full flex items-start justify-center lg:item-center">
+      <motion.img
+      initial = {{y:0}}
+      animate ={{y:[-10,10,10]}}
+      transition = {{
+        repeat:Infinity,
+        duration:2,
+        ease:"linear",
+      }}
+      src = {Hero}
+      className="w-auto h-auto object-contain"
+      />
+    </div>
+    </div>
+    </section>
+  </>
+ 
 };
 
 export default Home;
